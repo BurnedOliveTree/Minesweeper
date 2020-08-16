bd5f1a88ff36a767c210f0e79592e578e44c36bf
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2020-08-16 11:17:47 +0200

    added basic command line arguments handling

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Minesweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+            if (args.Length > 0)
+            {
+                a += args[0];
+            }
+            Console.WriteLine("Hello "+a+"!");
+        }
+    }
+}
