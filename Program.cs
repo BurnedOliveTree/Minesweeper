0704ffb3fcf8dd321d3bfca4689f2e16557f051b
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2020-08-17 23:29:06 +0200

    simple polishing, also removed the option to lose instantly

--- Program.cs
+++ Program.cs
@@ -1,58 +1,59 @@
 ﻿using System;
+using System.Net;
 
 namespace Game
 {
     class Minesweeper
     {
-        private int size;
-        private int mines;
-        private int[,] values;
-        private bool[,] show;
+        private readonly int _size;
+        private readonly int _mines;
+        private readonly int[,] _values;
+        private readonly bool[,] _show;
         
-        Minesweeper(int argSize, int argMines)
+        private Minesweeper(int argSize, int argMines)
         {
-            size = argSize;
-            mines = argMines;
-            values = new int[size+2, size+2];
-            show = new bool[size+2, size+2];
+            _size = argSize;
+            _mines = argMines;
+            _values = new int[_size+2, _size+2];
+            _show = new bool[_size+2, _size+2];
         }
         private void CreateVoid()
         {
-            for (int i = size + 1; i >= 0; i--)
+            for (int i = _size + 1; i >= 0; i--)
             {
-                values[0, i] = 10;
-                values[size+1, i] = 10;
-                values[i, 0] = 10;
-                values[i, size+1] = 10;
+                _values[0, i] = 10;
+                _values[_size+1, i] = 10;
+                _values[i, 0] = 10;
+                _values[i, _size+1] = 10;
             }
         }
-        private void CreateMines()
+        private void CreateMines(int y, int x)
         {
             Random rnd = new Random();
             int tempMines = 0;
-            while (tempMines < mines)
+            while (tempMines < _mines)
             {
                 int c = 0, r = 0;
                 do
                 {
                     c = rnd.Next() % 10 + 1;
                     r = rnd.Next() % 10 + 1;
-                } while (values[c, r] == 9);
-                values[c, r] = 9;
+                } while (_values[c, r] == 9 || y == c && x == r);
+                _values[c, r] = 9;
                 for (int i = -1; i < 2; ++i)
                     for (int j = -1; j < 2; ++j)
                     {
                         if (i == 0 && j == 0)
                             continue;
-                        if (values[c + i, r + j] != 9)
-                            values[c + i, r + j] += 1;
+                        if (_values[c + i, r + j] != 9)
+                            _values[c + i, r + j] += 1;
                     }
                 tempMines++;
             }
         }
         private string ToEqualString(int number)
         {
-            int desiredDigitAmount = size / 10;
+            int desiredDigitAmount = _size / 10;
             int startingDigitAmount = number / 10;
             string result = " ";
             for (int i = desiredDigitAmount - startingDigitAmount; i > 0; --i)
@@ -65,34 +66,34 @@ namespace Game
             switch (number)
             {
                 case 0:
-                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
                     break;
                 case 1:
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    break;
-                case 2:
                     Console.ForegroundColor = ConsoleColor.Green;
                     break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
                 case 3:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    break;
-                case 4:
-                    Console.ForegroundColor = ConsoleColor.DarkBlue;
-                    break;
-                case 5:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    break;
-                case 6:
-                    Console.ForegroundColor = ConsoleColor.DarkCyan;
-                    break;
-                case 7:
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     break;
-                case 8:
+                case 4:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case 5:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case 6:
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    break;
+                case 7:
                     Console.ForegroundColor = ConsoleColor.DarkYellow;
                     break;
+                case 8:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
                 case 9:
-                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
                     break;
             }
             Console.Write(ToEqualString(number));
@@ -103,55 +104,64 @@ namespace Game
         {
             //Sara tu byla
             string hidden = "";
-            for (int i = size / 10; i >= 0; --i)
+            for (int i = _size / 10; i >= 0; --i)
             {
                 Console.Write(" ");
                 hidden += " ";
             }
             hidden = hidden + "H";
             Console.Write(" ");
-            for (int i = 1; i < size + 1; ++i)
+            for (int i = 1; i < _size + 1; ++i)
                 Console.Write(ToEqualString(i));
             Console.WriteLine();
-            for (int i = 1; i < size + 1; ++i)
+            for (int i = 1; i < _size + 1; ++i)
             {
                 Console.Write(ToEqualString(i));
-                for (int j = 1; j < size + 1; ++j)
+                for (int j = 1; j < _size + 1; ++j)
                 {
-                    if (show[i, j])
-                        WriteColour(values[i, j]);
+                    if (_show[i, j])
+                        WriteColour(_values[i, j]);
                     else
                         Console.Write(hidden);
                 }
                 Console.WriteLine();
             }
         }
+        private static int InputInt()
+        {
+            int number;
+            while(true)
+            {
+                try
+                {
+                    number = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: Expected a single integer");
+                    continue;
+                }
+            }
+            return number;
+        }
+        private void FirstInput()
+        {
+            Console.WriteLine("Enter coordinates: (c/r)");
+            int c = InputInt(), r = InputInt();
+            CreateMines(c, r);
+            _show[c, r] = true;
+            if (_values[c, r] == 0)
+                Spread(c, r);
+        }
         private bool Input()
         {
             Console.WriteLine("Enter coordinates: (c/r)");
-            int c, r;
-            try
-            {
-                c = int.Parse(Console.ReadLine());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("An example of correct coordinates:\n5\n5");
-                c = int.Parse(Console.ReadLine());
-            }
-            try
-            {
-                r = int.Parse(Console.ReadLine());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("An example of correct coordinates:\n5\n5");
-                r = int.Parse(Console.ReadLine());
-            }
-            show[c, r] = true;
-            if (values[c, r] == 9)
+            int c = InputInt(), r = InputInt();
+            _show[c, r] = true;
+            if (_values[c, r] == 9)
                 return true;
-            if (values[c, r] == 0)
+            if (_values[c, r] == 0)
                 if (Spread(c, r))
                     return true;
             return false;
@@ -159,12 +169,12 @@ namespace Game
         private bool Check()
         {
             int covered = 0;
-            for (int i = 1; i < size + 1; ++i)
-                for (int j = 1; j < size + 1; ++j)
+            for (int i = 1; i < _size + 1; ++i)
+                for (int j = 1; j < _size + 1; ++j)
                 {
-                    if (!show[i, j])
+                    if (!_show[i, j])
                         covered++;
-                    if (covered > mines)
+                    if (covered > _mines)
                         return false;
                 }
             return true;
@@ -176,12 +186,12 @@ namespace Game
                 {
                     if (i == 0 && j == 0)
                         continue;
-                    if (!show[c + i, r + j])
+                    if (!_show[c + i, r + j])
                     {
-                        show[c + i, r + j] = true;
-                        if (values[c + i, r + j] == 0)
+                        _show[c + i, r + j] = true;
+                        if (_values[c + i, r + j] == 0)
                             Spread(c + i, r + j);
-                        else if (values[c + i, r + j] == 9)
+                        else if (_values[c + i, r + j] == 9)
                             return true;
                     }
                 }
@@ -190,7 +200,8 @@ namespace Game
         private bool Core()
         {
             CreateVoid();
-            CreateMines();
+            Print();
+            FirstInput();
             while (!Check())
             {
                 Print();
@@ -199,12 +210,15 @@ namespace Game
             }
             return true;
         }
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            int size = 10;
+            int size = 12, mines = 12;
             if (args.Length > 0)
+            {
                 size = Convert.ToInt32(args[0]);
-            Minesweeper game = new Minesweeper(size, 10);
+                mines = Convert.ToInt32(args[1]);
+            }
+            Minesweeper game = new Minesweeper(size, mines);
             if (game.Core())
                 Console.WriteLine("Congratulations! You won!");
             else
