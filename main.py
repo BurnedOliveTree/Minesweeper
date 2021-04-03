e580a761f782f1df1d68899266a7f9a4bc444e2d
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2021-04-03 12:05:47 +0200

    made a simple GUI with buttons and connected it to board object

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# import PySimpleGUI as sg
+import PySimpleGUI as sg
 import numpy as np
+
+sg.theme('DarkAmber')
 
 class Board:
     def __init__(self, size, mines_amount):
@@ -34,6 +36,13 @@ class Board:
                 except IndexError:
                     pass
     
+    def input(self, coords):
+        self.show[coords[0]][coords[1]] = True
+        if self.board[coords[0]][coords[1]] == 9:
+            return -1
+        else:
+            return 0
+
     def __str__(self):
         result = ''
         for x in range(self.size[0]):
@@ -47,3 +56,21 @@ class Board:
 
 game = Board(10, 10)
 print(game)
+
+layout = [[sg.Button(str('H'), key=str(10*x+y)) for x in range(game.size[0])] for y in range(game.size[1])]
+
+window = sg.Window("Minesweeper", layout, finalize=True)
+
+while True:
+    event, values = window.read()
+    try:
+        coords = int(event)
+        result = game.input((coords//10, coords%10))
+        window[str(coords)].update(text=str(game.board[coords//10][coords%10]))
+        if result == -1:
+            break
+    except TypeError:
+        if event == sg.WIN_CLOSED:
+            break
+
+window.close()
