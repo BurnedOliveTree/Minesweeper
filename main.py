114b5bf7a21f47870240de522caac52845eea4c3
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2021-04-04 11:11:02 +0200

    implemented Input mechanic

--- main.py
+++ main.py
@@ -14,50 +14,59 @@ class Board:
             for y in range(self.size[1]):
                 if self.board[x][y] == 9:
                     continue
-                try:
-                    if self.board[x-1][y-1] == 9:
-                        self.board[x][y] += 1
-                    if self.board[x-1][y] == 9:
-                        self.board[x][y] += 1
-                    if self.board[x-1][y+1] == 9:
-                        self.board[x][y] += 1
-                    if self.board[x][y-1] == 9:
-                        self.board[x][y] += 1
-                    if self.board[x][y] == 9:
-                        self.board[x][y] += 1
-                    if self.board[x][y+1] == 9:
-                        self.board[x][y] += 1
-                    if self.board[x+1][y-1] == 9:
-                        self.board[x][y] += 1
-                    if self.board[x+1][y] == 9:
-                        self.board[x][y] += 1
-                    if self.board[x+1][y+1] == 9:
-                        self.board[x][y] += 1
-                except IndexError:
-                    pass
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        if i == 0 and j == 0:
+                            continue
+                        if x == 0 and i == -1 or x == self.size[0]-1 and i == 1:
+                            continue
+                        if y == 0 and j == -1 or y == self.size[1]-1 and j == 1:
+                            continue
+                        if self.board[x+i][y+j] == 9:
+                            self.board[x][y] += 1
+            self.board[x] = tuple(self.board[x])
     
     def input(self, coords):
-        self.show[coords[0]][coords[1]] = True
-        if self.board[coords[0]][coords[1]] == 9:
-            return -1
-        else:
-            return 0
+        if not self.show[coords[0]][coords[1]]:
+            self.show[coords[0]][coords[1]] = True
+        if self.board[coords[0]][coords[1]] == 0:
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if i == 0 and j == 0:
+                        continue
+                    if coords[0] == 0 and i == -1 or coords[0] == self.size[0]-1 and i == 1:
+                        continue
+                    if coords[1] == 0 and j == -1 or coords[1] == self.size[1]-1 and j == 1:
+                        continue
+                    if not self.show[coords[0]+i][coords[1]+j]:
+                        self.show[coords[0]+i][coords[1]+j] = True
+                        if self.board[coords[0]+i][coords[1]+j] == 0:
+                            self.input((coords[0]+i, coords[1]+j))
+                        elif self.board[coords[0]+i][coords[1]+j] == 9:
+                            return -1
+        return 0
 
     def __str__(self):
         result = ''
         for x in range(self.size[0]):
             for y in range(self.size[1]):
-                if self.show[x][y]:
+                if not self.show[x][y]:
                     result += ' ' + str(self.board[x][y])
                 else:
                     result += ' H'
             result += '\n'
         return result
 
+def update_window(window, board):
+    for x in range(board.size[0]):
+        for y in range(board.size[1]):
+            if board.show[x][y]:
+                window[str(10*x+y)].update(text=str(game.board[x][y]))
+
 game = Board(10, 10)
 print(game)
 
-layout = [[sg.Button(str('H'), key=str(10*x+y)) for x in range(game.size[0])] for y in range(game.size[1])]
+layout = [[sg.Button(str('H'), key=str(10*x+y)) for y in range(game.size[1])] for x in range(game.size[0])]
 
 window = sg.Window("Minesweeper", layout, finalize=True)
 
@@ -66,7 +75,7 @@ while True:
     try:
         coords = int(event)
         result = game.input((coords//10, coords%10))
-        window[str(coords)].update(text=str(game.board[coords//10][coords%10]))
+        update_window(window, game)
         if result == -1:
             break
     except TypeError:
